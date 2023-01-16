88faa8e39e592fd8d1d1eb08d65b6bb63978b338
Arshak <arshakbabayan52@gmail.com>
2023-01-16 22:36:28 +0400

    finish 2 part project

--- JS/script.js
+++ JS/script.js
@@ -27,7 +27,7 @@ const personalMovieDB = {
 
 
 for (let i = 0; i < 2; i++){
-    const a = prompt('Ваш первый фильм', ''),
+    const a = prompt('Ваш фильм', ''),
           b = prompt('Ваша оценка', '10');
     
           if (a != null && b != null && a != '' && b != '' && a.length < 50){
@@ -37,6 +37,16 @@ for (let i = 0; i < 2; i++){
             console.log('eror');
             i--;
           }
+}
+
+if (personalMovieDB.count < 10){
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30){
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
 }
 
 console.log(personalMovieDB);
